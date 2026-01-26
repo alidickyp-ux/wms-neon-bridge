097af6cb73cd99244ad460b3224dbacfc0ad17b4
alidickyP <alidickyp@gmail.com>
2026-01-26 10:35:20 +0700

    Update inventory.js

--- api/inventory.js
+++ api/inventory.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
   res.setHeader('Cache-Control', 'no-store');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, cache-control');
 
   if (req.method === 'OPTIONS') return res.status(200).end();
 
